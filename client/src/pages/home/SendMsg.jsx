91ae7e72a5b7a7ed7aea3ab94e2e27fbf8bb841a
RAJVEERSINGH58 <148183788+RAJVEERSINGH58@users.noreply.github.com>
2025-07-29 15:34:43 +0530

    Update SendMsg.jsx

--- client/src/pages/home/SendMsg.jsx
+++ client/src/pages/home/SendMsg.jsx
@@ -13,13 +13,24 @@ const SendMsg = () => {
   }
 
   const handleSendMsg = async () => {
-    if (selectedUser) {
-      dispatch(sendMessageThunk({ receiverId: selectedUser?._id, message: msg }));
-      setMsg(''); 
-    } else {
-      toast.error("Please select a user to send a message to.");
-    }
+  console.log("Selected User:", selectedUser);           // Debug log
+  console.log("Message to send:", msg);                  // Debug log
+
+  if (!selectedUser || !selectedUser._id) {
+    return toast.error("Please select a user to send a message to.");
   }
+
+  if (!msg.trim()) {
+    return toast.error("Message cannot be empty.");
+  }
+
+  dispatch(sendMessageThunk({
+    receiverId: selectedUser._id,
+    message: msg.trim(),
+  }));
+
+  setMsg('');
+};
 
   return (
     <div className="w-full flex gap-2 pb-2">
