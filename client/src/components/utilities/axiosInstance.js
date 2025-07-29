0111c54ad5594d9afae4e08326b2d558fee5f521
RAJVEERSINGH58 <148183788+RAJVEERSINGH58@users.noreply.github.com>
2025-07-29 15:58:42 +0530

    Update axiosInstance.js

--- client/src/components/utilities/axiosInstance.js
+++ client/src/components/utilities/axiosInstance.js
@@ -5,7 +5,7 @@ const DB_URL = import.meta.env.VITE_DB_URL;
 export const axiosInstance = axios.create({
   baseURL: DB_URL,
   withCredentials : true,
-  timeout : 1000,
+  timeout : 10000,
   headers: {
     'Content-Type': 'application/json',
   }
