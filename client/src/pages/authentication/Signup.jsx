8f6952bed4ebc0ca745fd2b6deb60bc5156253f5
RAJVEERSINGH58 <148183788+RAJVEERSINGH58@users.noreply.github.com>
2025-07-29 13:27:24 +0530

    Update Signup.jsx

--- client/src/pages/authentication/Signup.jsx
+++ client/src/pages/authentication/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useEffect} from "react";
 import { FaUser, FaKey } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
